acd72d70b862d14d9afda1babd760733d82ce65d
Murat Kudainetov <murat.kudainetov@gmail.com>
2019-11-05 00:48:33 +0300

    реализовал счетчики с использованием redux. разбил редюсер, использую actionCreator

---
+++ src/redux/reducers/counter1.js
@@ -0,0 +1,26 @@
+import {ADD, ADD_NUMBER, SUB} from "../actions/actionTypes";
+
+const initialState = {
+  counter: 0
+};
+
+function counter1(state = initialState, action) {
+  switch (action.type) {
+    case ADD:
+      return {
+        counter: state.counter + 1
+      };
+    case SUB:
+      return {
+        counter: state.counter - 1
+      };
+    case ADD_NUMBER:
+      return {
+        counter: state.counter + action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter1;
